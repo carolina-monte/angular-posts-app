81c32cff45e34649971c703c1fc992d231d35d97
Carolina Monte <carolina.monte@intraway.com>
2019-12-05 16:30:23 -0300

    Added service and some other changes

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostService } from '../post.service';
+
 
 @Component({
   selector: 'app-post-create',
@@ -7,10 +10,17 @@ import { Component } from '@angular/core';
 })
 
 export class PostCreateComponent {
-  enteredValue ='';
-  newPost = 'No content';
+  enteredContent = '';
+  enteredTitle =  '';
 
-  onAddPost(){
-    this.newPost = this.enteredValue;
+  constructor(public postService: PostService){
+
+ }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postService.addPost(form.value.title, form.value.content);
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-post-list',
@@ -7,8 +11,29 @@ import { Component } from '@angular/core';
 
 })
 
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
+/*   posts = [
+    { title: 'First Post', content: 'This is the first post\'s content' },
+    { title: 'Second Post', content: 'This is the second post\'s content' },
+    { title: 'Third Post', content: 'This is the third post\'s content' }
+  ]; */
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
+  constructor(public postsService: PostService) {
 
+  }
 
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) => {
+      this.posts = posts;
+
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
